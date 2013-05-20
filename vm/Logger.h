5e6d1f8e4779cfb10e67efcd4131ce4255ac61d4
kiyotakaOTA <kiyotaka.04@gmail.com>
2013-05-20 18:11:38 +0900

    Update Logger.h
    #define -> enum
    マクロを列挙型変数に変更．

--- vm/Logger.h
+++ vm/Logger.h
@@ -1,17 +1,21 @@
+enum LogType
+{
+	LOG_NO = 0,
+	LOG_TURN = 1,
+	LOG_PWM = 2,
+	LOG_TARGET_ANGLE = 3,
+	LOG_LIGHT_MIN = 4,
+	LOG_MOTOR_COUNT = 5,
+	LOG_SONAR = 6,
+	LOG_STATE = 7,
+	LOG_DT = 8,
+	LOG_BALANCE_TAIL = 9,
+	LOG_LOOP = 10
+};
+
 typedef struct tag_Logger{
 
-
-#define LOG_TURN 1
-#define LOG_PWM 2
-#define LOG_TARGET_ANGLE 3
-#define LOG_LIGHT_MIN 4
-#define LOG_MOTOR_COUNT 5
-#define LOG_SONAR 6
-#define LOG_STATE 7
-#define LOG_DT 8
-#define LOG_BALANCE_TAIL 9
-#define LOG_LOOP 10
-	byte type;
+	enum LogType type;
 
 } Logger_t;
 
