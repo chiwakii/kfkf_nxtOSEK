9da2c9d59376f0e2b7e5a4e5aefae4ef6c9a0a3a
fukuda lab shared PC <kawasakitoshiya@gmail.com>
2013-05-23 14:19:48 +0900

    実行ｈぁいるの追加

---
+++ exe/rxeflash.sh
@@ -0,0 +1,4 @@
+echo Executing NeXTTool to upload TASK_CYCLE_OLD.rxe...
+ /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=TASK_CYCLE_OLD.rxe
+ /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=TASK_CYCLE_OLD.rxe
+echo NeXTTool is terminated.
