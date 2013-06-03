3e0c5ba48ba2f191e41c5f96ed781cc5a3078e59
KiyotakaOTA <kiyotaka.04@gmail.com>
2013-06-03 20:45:19 +0900

    Addition and Delete
    add spaces and new lines and delete comments

--- vm/task_cycle.c
+++ vm/task_cycle.c
@@ -149,16 +149,13 @@ TASK(TaskActuator)
 
 	if(controller.pid_on == 1)	// Linetrace border between black and white
 	{
-		sensor.light = ecrobot_get_light_sensor(LIGHT_SENSOR);
 		before = standard;
 		standard = sensor.light - sensor.gray;					// AAeA竏羨?CiAenAaAE
 		controller.integral += (standard - before)/2.0 * 0.004;	
 
-		//controller.turn = 1 * standard *100 / (black-white) + 1 * controller.integral / (black-white) * 100 + 1 * (sensor.light-sensor.prev_light_value) / (black-white) * 100;	//EoaAouAAﾂｧERaAAAE
-
 		controller.turn = controller.P_gain * standard *100 / (sensor.black-sensor.white) + controller.I_gain * integral / (sensor.black-sensor.white) * 100 + controller.D_gain * (sensor.light-sensor.prev_light_value) / (sensor.black-sensor.white) * 100;	// EoaAouAAﾂｧERaAAAE
 		sensor.prev_light_value = sensor.light;		//1?AﾂｧAac?AEEoe?Ca?Ai?CiE窶�A\AE
-		controller.forward_power=controller.speed;
+		controller.forward_power = controller.speed;
 	}
 	else if(controller.wg_pid_on == 1)	// Linetrace border between gray and white
 	{
@@ -173,12 +170,12 @@ TASK(TaskActuator)
 	}
 	else
 	{
-		//controller.turn=0;
-		//controller.forward_power=controller.speed;
+
 	}
 
 	
-	if(controller.balance_on == 1){
+	if(controller.balance_on == 1)
+	{
 		balance_control(					//?Ee?Ec?E??CﾎｼAPI?AAAeo?A?Aa竏ｫ?AAE
 			(float)controller.forward_power,		//AacEA?AEeAaaEAAAeﾎｩ窶ｰaﾂｧ(-100(Ae?EoaAouEuAAﾂｧs)?100(A竏堕ｶEoaAouEuAAﾂｧs))
 			(float)controller.turn,			//EoaAouAeﾎｩ窶ｰaﾂｧ(-100?100)
@@ -196,41 +193,45 @@ TASK(TaskActuator)
 		nxt_motor_set_speed(NXT_PORT_C, (S8)(pwm_L*controller.left_motor_rate), 1);		//?E��Eo?Co?AﾂｴEaAEaﾂｧ?CiAAE?AAE
 		nxt_motor_set_speed(NXT_PORT_B, (S8)(pwm_R*controller.right_motor_rate), 1);
 	
-	}else if(controller.tail_on==1){
-		
+	}
+	else if(controller.tail_on==1)
+	{
 		U32 _tail_ang = nxt_motor_get_count(TAIL_MOTOR);
 		U8 _tail_speed;
 	
-		if(_tail_ang>=0){
+		if(_tail_ang>=0)
+		{
 			_tail_speed = (U8)(10.0/9.0*(controller.tail_ang - _tail_ang)+controller.tail_speed_offset);
-		}else{
+		}
+		else
+		{
 			_tail_speed=10;
 		}
 	
 		
 
-		if(_tail_ang > controller.tail_ang+1){
+		if(_tail_ang > controller.tail_ang+1)
+		{
 			nxt_motor_set_speed(TAIL_MOTOR,-50,1);
-		
-			
-		}else if(_tail_ang < controller.tail_ang-1){
-			nxt_motor_set_speed(TAIL_MOTOR,_tail_speed,1);
-		
-		
-		}else{
-			
-			nxt_motor_set_speed(TAIL_MOTOR,0,1);
-				
 		}
+		else if(_tail_ang < controller.tail_ang-1)
+		{
+			nxt_motor_set_speed(TAIL_MOTOR,_tail_speed,1);
+		}
+		else
+		{
+			nxt_motor_set_speed(TAIL_MOTOR,0,1);
+		}
+		
 		tail_run_turn2pwm(
 				(S16) controller.tail_run_speed,
 				(float) controller.turn,
 				&pwm_L,
 				&pwm_R
 		);
+
 		nxt_motor_set_speed(RIGHT_MOTOR,(S8)(pwm_R*controller.right_motor_rate),1);
 		nxt_motor_set_speed(LEFT_MOTOR,(S8)(pwm_L*controller.left_motor_rate),1);
-
 	}
 
 
